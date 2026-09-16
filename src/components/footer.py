04ccb9e80503ff99450e5f9159022d5f9c3b2587
Ayushbms05 <princebpm525@gmail.com>
2026-09-16 22:33:24 +0530

    teacher authentication

--- src/components/footer.py
+++ src/components/footer.py
@@ -4,7 +4,16 @@ def footer_home():
     logo_url = "https://i.ibb.co/YTYGn5qV/logo.png"
     st.markdown(f"""
     <div style="margin-top: 2rem; display:flex; gap:6px; justify-content:center; item-align:center;">
-        <p style="font-weight:bold;" >Created by Ayush Kumar</p>
+        <p style="font-weight:bold; color: white;" >Created by Ayush Kumar</p>
+    </div>        
+
+    """, unsafe_allow_html=True)
+
+def footer_dashboard():
+    logo_url = "https://i.ibb.co/YTYGn5qV/logo.png"
+    st.markdown(f"""
+    <div style="margin-top: 2rem; display:flex; gap:6px; justify-content:center; item-align:center;">
+        <p style="font-weight:bold; color: black;" >Created by Ayush Kumar</p>
     </div>        
 
     """, unsafe_allow_html=True)
--- src/ui/base_layout.py
+++ src/ui/base_layout.py
@@ -3,7 +3,7 @@ def style_background_home():
     st.markdown("""
             <style>
                 .stApp{
-                    background: #5865F2 !important
+                    background: #5865F2 !important;
                 }
 
                 .stApp div[data-testid="stColumn"]{
@@ -20,7 +20,10 @@ def style_background_dashboard():
     st.markdown("""
             <style>
                 .stApp{
-                    background: #E0E3FF !important
+                    background: #E0E3FF !important;
+                }
+                #register-teacher {
+                    color: black !important;
                 }
             </style>
                 """, unsafe_allow_html=True)
